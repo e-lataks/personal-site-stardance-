a38b9b00888c4ee5b7adac41999ed5eae41c1378
e-lataks <emiklataks600@gmail.com>
2026-07-02 15:28:19 +0300

    feat: build first complete version of portfolio homepage including Flask routing, base template, custom navbar, hero section, current focus progress bars, project showcase cards and improved styling

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("baze.html")
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return render_template(about.html)
+    return render_template("about.html")
 
 @app.route("/projects")
 def projects():
@@ -17,3 +17,6 @@ def projects():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
